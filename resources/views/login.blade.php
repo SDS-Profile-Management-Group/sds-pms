276bfb6f92ed474128bb0d86cd88eb7d9574df47
Afwan-Rezal <21b6027@ubd.edu.bn>
2024-09-26 12:49:46 +0800

    Introduce index and login page

---
+++ resources/views/login.blade.php
@@ -0,0 +1,20 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Profile Management System - Login</title>
+</head>
+<body>
+    <div class="header">
+
+    </div>
+
+    <div class="login-page">
+        <form action="/login" method="POST">
+            @csrf
+
+        </form>
+    </div>
+</body>
+</html>
