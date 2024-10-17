b5c9f5c023ab9e03a5c22999291ad677829d9cd2
Afwan-Rezal <21b6027@ubd.edu.bn>
2024-10-17 11:48:34 +0800

    Refractor function into ProfileController

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Profile;
+
+class ProfileController extends Controller
+{
+    public function enterName(Request $request){
+        $incomingFields = $request->validate([
+            "full_name" => "required|regex:/^[a-zA-Z\s]+$/", // Allows letters and spaces
+        ]);
+
+        // ? Ensure we're updating the profile with the right username
+        $profile = Profile::firstOrNew([
+            'username' => auth()->user()->username,  
+        ]);
+    
+        // Update the full name if the profile exists or create a new one
+        $profile->full_name = $incomingFields['full_name'];
+        $profile->save(); // Save the profile
+    
+        // Redirect with success message
+        return redirect('/home');
+    }
+}
