5b0c73d23d4219e40ede5a4c1619d85f438268c0
Afwan-Rezal <21b6027@ubd.edu.bn>
2025-03-13 11:45:51 +0800

    Revert "To revert"
    This reverts commit b9ac5270eeca91dc4d90de1b97fb704d3a36bb0e.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ config('app.name', 'Laravel') }}</title>
-    {{-- <script src="https://cdn.tailwindcss.com"></script> <!-- Include TailwindCSS --> --}}
-    @vite('resources/css/app.css')
+    <script src="https://cdn.tailwindcss.com"></script> <!-- Include TailwindCSS -->
 </head>
 <body class="bg-gray-100">
     <!-- Navbar -->
